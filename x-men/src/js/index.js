71020f1b00d2254f3cb20f505c468d625ab39f90
Igor Souza <103651578+Isouz@users.noreply.github.com>
2023-07-11 20:16:17 -0300

    Update index.js

--- x-men/src/js/index.js
+++ x-men/src/js/index.js
@@ -6,23 +6,35 @@ personagens.forEach((personagem) => {
             window.scrollTo({top: 0, behavior: 'smooth'})
         }
 
-        const personagemSelecionado = document.querySelector('.selecionado')
-
-        personagemSelecionado.classList.remove('selecionado')
+        removerSelecao()
 
         personagem.classList.add('selecionado')
         
-        const imagemPersonagemGrande = document.querySelector('.personagem-grande')
+        alterarPersonagemSelecionado(personagem)
 
-        const idPersonagem = personagem.attributes.id.value
-   
-        imagemPersonagemGrande.src = `src/imagens/card-${idPersonagem}.png`
+        altNomeSelecionado(personagem)
 
-        const nomePersonagem = document.getElementById('nome-personagem')
-
-        nomePersonagem.innerText = personagem.getAttribute('data-name')
-
-        const descricaoPersonagem = document.getElementById("descricao-personagem")
-        descricaoPersonagem.innerText = personagem.getAttribute('data-description')
+        altDescricao(personagem)
     })
 })
+
+function altDescricao(personagem) {
+    const descricaoPersonagem = document.getElementById("descricao-personagem")
+    descricaoPersonagem.innerText = personagem.getAttribute('data-description')
+}
+
+function altNomeSelecionado(personagem) {
+    const nomePersonagem = document.getElementById('nome-personagem')
+    nomePersonagem.innerText = personagem.getAttribute('data-name')
+}
+
+function alterarPersonagemSelecionado(personagem) {
+    const imagemPersonagemGrande = document.querySelector('.personagem-grande')
+    const idPersonagem = personagem.attributes.id.value
+    imagemPersonagemGrande.src = `src/imagens/card-${idPersonagem}.png`
+}
+
+function removerSelecao() {
+    const personagemSelecionado = document.querySelector('.selecionado')
+    personagemSelecionado.classList.remove('selecionado')
+}
